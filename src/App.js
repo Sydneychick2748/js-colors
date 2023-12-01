527e3e6a3831d8a302429f70a0ee170e1aea966e
ana <ana@anniecannons.com>
2023-12-01 11:39:26 -0800

    added notes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+// importing the component to the parent app
 import Color from '../src/Components/Color';
 import { useState } from 'react';
-
+//  this is the fucntion of the parent app
 function App() {
+  // data that holds key value pairs of an array of data 
   let data = [
     { color: "red", value: "#f00" },
     { color: "green", value: "#0f0" },
@@ -13,11 +15,14 @@ function App() {
     { color: "yellow", value: "#ff0" },
     { color: "black", value: "#000" },
   ];
-
+   
+//  usestate variable that is set to null 
   const [clickedIndex, setClickedIndex] = useState(null);
 
+  // fucntion for the click that passes a parameter for the click function 
   function clickHandler(index) {
     console.log("clicked");
+    // updating the usestate variable from null and passinf in the paramater from the fucntion 
     setClickedIndex(index);
     console.log(index, "index")
   }
@@ -26,7 +31,10 @@ function App() {
 
   return (
     <div className="App">
-  
+  {/* passing the component of to the parent in jsx */}
+
+  {/* setting props for the component so the child can acess the properties  */}
+ {/* the props for the data so the child can access the data , the useState needs to be passeed to the child and the whole click fucntion needs to be passed to the child with a anoymous arrow fucntion which needs to have parameters to pass in  */}
         <Color  dataProp={data} onClickProps={(index) => clickHandler(index)} clickedIndexProps={clickedIndex} />
     
     </div>
--- src/Components/Color.js
+++ src/Components/Color.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+// this is the function for the color component and it has props passed in from app
 function Color(props) {
 
  
@@ -7,16 +7,21 @@ function Color(props) {
  console.log(props, "props")
   return (
     <>
+    {/* the map is looping threw the data which we passed in from props and is mapping threw each object in the data array and the key for each data object  */}
    {props.dataProp.map((data, i) => {
         return (
           <div
+        //   the key for the loop for js is important 
             key={i}
-            onClick={()=>props.onClickProps(i)}
+            // the onclick on the div is passed as a anonamysoe fucntion so it dose not fire the fucntion call before the item is clicked and a parameter is passed in the fucntion so it knows were the click is in the data
+            // onClick={()=>props.onClickProps(i)}
+            // the style tag is in line and it is a ternary passing in the props of the original usestate and finding out if it is truly the index in the map is clicked then to change the color passing in the props in the data to access the information 
             style={{
                 color: props.clickedIndexProps === i ? data.value : "black",
                 
               }}
           >
+            {/* this is a paragraph that dispalys in the jsx and it is grabbing the props in the array of data from the parent  */}
             <p>{data.color}</p>
             <p>{data.value}</p>
           </div>
